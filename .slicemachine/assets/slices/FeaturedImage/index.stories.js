1f7d4b715d3a01c9655d479312e906208dc9601e
TolaAbiodun <adetolaabiodun@outlook.com>
2022-01-18 13:22:42 +0100

    created Home Page

--- .slicemachine/assets/slices/FeaturedImage/index.stories.js
+++ .slicemachine/assets/slices/FeaturedImage/index.stories.js
@@ -5,5 +5,5 @@ export default {
 }
 
 
-export const _DefaultSlice = () => <MyComponent slice={{"variation":"default-slice","name":"Default slice","slice_type":"featured_image","items":[],"primary":{"title":[{"type":"heading1","text":"Transition killer convergence","spans":[]}],"headline":[{"type":"heading3","text":"Recontextualize mission-critical infomediaries","spans":[]}],"link":{"link_type":"Web","url":"http://google.com"},"linkLabel":"engineer distributed vortals","featuredImage":{"dimensions":{"width":727,"height":402},"alt":"Placeholder image","copyright":null,"url":"https://images.unsplash.com/photo-1544731612-de7f96afe55f?w=727&h=402&fit=crop"}},"id":"_DefaultSlice"}} />
+export const _DefaultSlice = () => <MyComponent slice={{"variation":"default-slice","name":"Default slice","slice_type":"featured_image","items":[],"primary":{"title":[{"type":"heading1","text":"Reintermediate open-source metrics","spans":[]}],"headline":[{"type":"heading3","text":"Maximize innovative content","spans":[]}],"link":{"link_type":"Web","url":"https://prismic.io"},"linkLabel":"maximize enterprise e-business","featuredImage":{"dimensions":{"width":727,"height":402},"alt":"Placeholder image","copyright":null,"url":"https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?w=727&h=402&fit=crop"}},"id":"_DefaultSlice"}} />
 _DefaultSlice.storyName = 'Default slice'
